6413f4c4a0d7c2aa6c0ae250a2e2d2b49dc57eaa
Aditya Sharma <123091289+adsh16@users.noreply.github.com>
2024-09-01 19:12:00 +0530

    imported os

--- leetcode_submission_bot.py
+++ leetcode_submission_bot.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import os
 
 # Set up Chrome options
 chrome_options = Options()
